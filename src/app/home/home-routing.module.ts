f86eab212c55d851d0e1b777486414779a36af98
Diego Morales <diego.mm09@hotmail.com>
2022-12-05 14:04:32 -0500

    Init

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home.component";
+import {ProfileComponent} from "./profile/profile.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'profile',
+        loadComponent: () => ProfileComponent
+      }
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
